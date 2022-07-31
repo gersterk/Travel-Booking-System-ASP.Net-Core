a926771adb67bb930bd94669c0366559b1055dec
Baris Dogan <barisdogan.biz@gmail.com>
2022-07-31 20:15:40 +0200

    Statistic ViewBags are assigned... Guides, Routes, Awards, Happy costumers

---
+++ Travel-Blog-Core/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Travel_Blog_Core.ViewComponents.Default
+{
+    //popular destinations statistics Component
+    public class _Statistics  :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //routes count
+            using var c = new Context();
+            ViewBag.v1 = c.Destinations.Count();
+
+            
+            ViewBag.v2 = c.Guides.Count();
+
+            ViewBag.v3 = "285";
+
+
+            return View();
+
+        }
+    }
+}
