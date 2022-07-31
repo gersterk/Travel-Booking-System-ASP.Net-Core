289153a7fe562746a68fd661e1e417731f5bc467
Baris Dogan <barisdogan.biz@gmail.com>
2022-07-31 17:39:04 +0200

    popular destinations controller and components have been created.. will be continued...

---
+++ Travel-Blog-Core/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Blog_Core.ViewComponents.Default
+{
+    public class _PopularDestinations : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+
+        }
+    }
+}
