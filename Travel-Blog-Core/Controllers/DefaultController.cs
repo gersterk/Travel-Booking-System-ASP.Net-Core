fc22f4f49011ecb7f3be425b88484c8214a9fd87
Baris Dogan <barisdogan.biz@gmail.com>
2022-07-31 16:10:35 +0200

    Controllers have been triggered. A Default View Added. MAIN html, css and javascript design have uploaded to the wwwroot

---
+++ Travel-Blog-Core/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Blog_Core.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
