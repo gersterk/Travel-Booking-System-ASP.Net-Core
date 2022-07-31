5e471d347161d85ab98bc2ebbf1ef23cda5d1e13
Baris Dogan <barisdogan.biz@gmail.com>
2022-07-31 15:41:42 +0200

    BusinessLayer Concrete Managers and Abstract Services have created Generically

---
+++ BusinessLayer/Abstract/IGenericServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Stores the signatures of business methods in Concrete Classes
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericServices<T>
+    {
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+
+        List<T> TGetList(); // It should be List because it must return something
+        T GetByID(int id);
+    }
+}
