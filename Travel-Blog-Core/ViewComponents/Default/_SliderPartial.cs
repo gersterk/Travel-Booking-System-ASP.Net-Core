d3903d73fe6dcb9c1551da8e54d30d869e4e6dd0
Baris Dogan <barisdogan.biz@gmail.com>
2022-07-31 17:21:37 +0200

    View Components have been created. Slider Partial has been set.

---
+++ Travel-Blog-Core/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Blog_Core.ViewComponents.Default
+{
+    public class _SliderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
