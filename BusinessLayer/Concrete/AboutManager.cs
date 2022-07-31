7167c181f5860a799f33c310ff487ee345556c23
Baris Dogan <barisdogan.biz@gmail.com>
2022-07-31 15:53:26 +0200

    Signatures have been filled... constructor methods have been created.

--- BusinessLayer/Concrete/AboutManager.cs
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,47 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    internal class AboutManager
+    public class AboutManager : IAboutService
     {
+        //Dependency Injection
+        IAboutDal _aboutDal;     
+
+
+       
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        //The constructor method
+
+        public About GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutDal.GetList();
+
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+
+                
+        }
     }
 }
