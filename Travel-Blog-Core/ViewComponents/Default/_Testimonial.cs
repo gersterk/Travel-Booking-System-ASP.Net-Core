26e4acf5c0b38e8b14e11b21cf3c7e998ce1cbc9
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-01 19:06:26 +0200

    Testimonial Business Manager has been established. Interfaces are implemented.

---
+++ Travel-Blog-Core/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Blog_Core.ViewComponents.Default
+{
+    public class _Testimonial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
