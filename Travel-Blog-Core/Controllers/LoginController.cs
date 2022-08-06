ac47a9532e064af63836717f05a62ebaa87333d2
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-06 13:12:04 +0200

    registration section have been added. almost done.

--- Travel-Blog-Core/Controllers/LoginController.cs
+++ Travel-Blog-Core/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Travel_Blog_Core.Models;
 
 namespace Travel_Blog_Core.Controllers
 {
@@ -23,8 +24,35 @@ namespace Travel_Blog_Core.Controllers
         }
 
         [HttpPost]
-        public async Task <IActionResult> SignUp(AppUser) 
+        public async Task <IActionResult> SignUp(UserRegisterViewModel p) 
         {
+            AppUser appUser = new AppUser()
+            {
+                Name = p.Name,
+                Surname = p.Surname,
+                Email = p.Email,
+                UserName = p.Username
+
+            };
+            if (p.Password == p.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
+
+
             return View();
         }
 
