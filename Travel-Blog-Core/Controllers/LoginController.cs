6eabceab91f6e8c15870e27c998ba627982569bb
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-06 10:31:36 +0200

    SignUp Index added

---
+++ Travel-Blog-Core/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Blog_Core.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+
+        }
+    }
+}
+
