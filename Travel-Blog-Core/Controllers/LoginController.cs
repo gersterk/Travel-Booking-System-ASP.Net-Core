70a28e7838e71179778b05756adf67929a03070e
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-06 12:59:38 +0200

    User Registration Model been created. Error Messages are assigned. Required and Compare notations have been added

--- Travel-Blog-Core/Controllers/LoginController.cs
+++ Travel-Blog-Core/Controllers/LoginController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Travel_Blog_Core.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
         [HttpGet]
         public IActionResult SignUp()
@@ -13,11 +22,11 @@ namespace Travel_Blog_Core.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult SignUp()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task <IActionResult> SignUp(AppUser) 
+        {
+            return View();
+        }
 
         [HttpGet]
         public IActionResult SignIn()
---
+++ Travel-Blog-Core/Models/UserRegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Travel_Blog_Core.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Please Insert Your Name")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please Insert Your Surname")]
+        public string Surname { get; set; }
+
+
+        [Required(ErrorMessage = "Please Insert Your Email Address")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please Insert a Username")]
+        public string Username { get; set; }
+
+
+        [Required(ErrorMessage = "Please Insert a Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please Confirm Your Password")]
+        [Compare("Password", ErrorMessage = "Passwords are not same")]
+        public string ConfirmPassword { get; set; }
+    }
+        
+}
