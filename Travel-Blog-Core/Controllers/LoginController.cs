d3170ca71470455f849c8aff462372c24318d504
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-06 19:09:55 +0200

    Destination Controller's location was wrong. removed. Signing In Error Messages, Rules has been added. Directions of Login has been established. PasswordSignInAsync

--- Travel-Blog-Core/Controllers/LoginController.cs
+++ Travel-Blog-Core/Controllers/LoginController.cs
@@ -11,11 +11,20 @@ namespace Travel_Blog_Core.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
+
+        //public LoginController(UserManager<AppUser> userManager)
+        //{
+        //    _userManager = userManager;
+        //}
+
+
 
         [HttpGet]
         public IActionResult SignUp()
@@ -60,6 +69,26 @@ namespace Travel_Blog_Core.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> SignIn(UserSignInModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Destination");
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
+
             return View();
 
         }
---
+++ Travel-Blog-Core/Models/UserSignInModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Travel_Blog_Core.Models
+{
+    public class UserSignInModel
+    {
+        [Required(ErrorMessage = "Please input your username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please input your password")]
+        public string Password { get; set; }
+
+     
+    }
+}
