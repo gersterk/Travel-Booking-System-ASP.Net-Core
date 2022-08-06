e9624258a1556e01993c16d3ef000d05281f2fe3
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-06 16:51:10 +0200

    _UserLayout separated into partoals: header, navbar...

--- Travel-Blog-Core/Areas/Member/Controllers/CommentController.cs
+++ Travel-Blog-Core/Areas/Member/Controllers/CommentController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Travel_Blog_Core.Areas.Member
+namespace Travel_Blog_Core.Areas.Member.Controllers
 {
+    [Area("Member")]
+    [AllowAnonymous]
     public class CommentController : Controller
     {
         public IActionResult Index()
