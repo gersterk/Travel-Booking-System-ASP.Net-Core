06b5054eae856a49a54a8aa196eb58925742e359
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-07 14:47:59 +0200

    Reservation Controller has been configured... Http Get and post ,IActionResults have been created for Previous and Current Reservations feature...

---
+++ Travel-Blog-Core/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,33 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Blog_Core.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+
+        }
+
+        public IActionResult MyPreviousResevertaion()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewReservation(Reservation p)
+        {
+            return View();
+        }
+    }
+}
