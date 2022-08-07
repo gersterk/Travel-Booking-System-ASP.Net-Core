bf675b4b672ecaaef7f3b9a7ea365eceece6aa56
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-07 14:38:49 +0200

    Essembly directories were missing... Have been solved. migrations have been added. [HAPPY]

--- EntityLayer/Concrete/Reservation.cs
+++ EntityLayer/Concrete/Reservation.cs
@@ -10,11 +10,11 @@ namespace EntityLayer.Concrete
     {
         public int ReservationID { get; set; }
         public int AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
         public string PersonCount { get; set; }
         public string Destination { get; set; }
         public DateTime ReservationDate { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
