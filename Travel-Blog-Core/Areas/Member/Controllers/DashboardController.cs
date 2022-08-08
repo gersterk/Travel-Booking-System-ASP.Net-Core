bd20fb12e7a2eefbe675b1e262467ae5f9b49429
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-08 19:18:08 +0200

    Dashboard bringing names, Images are assigned to ViewBags... Designed enhanced

--- Travel-Blog-Core/Areas/Member/Controllers/DashboardController.cs
+++ Travel-Blog-Core/Areas/Member/Controllers/DashboardController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Travel_Blog_Core.Areas.Member.Controllers
 {
     [Area("Member")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name +" " + values.Surname;
+            ViewBag.userImage = values.ImageUrl;
             return View();
         }
     }
