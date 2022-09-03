29c013eab4e556ed83bc4d9a3b9dad79f657d7c0
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-03 11:53:05 +0200

    Demo End

---
+++ Travel-Blog-Core/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Blog_Core.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
